5a76d6f2fbe0950231393085f41918b4b1cfa1b1
Jason Boutte <boutte.jason@gmail.com>
2020-02-22 03:16:34 -0800

    Fixes ability to set a url prefix

--- keycloak_forward/__init__.py
+++ keycloak_forward/__init__.py
@@ -4,6 +4,18 @@ from flask import request
 from flask import session
 from flask import Flask
 
+def fix_prefix(prefix):
+    if prefix == '':
+        return prefix
+
+    if prefix[0] != '/':
+        prefix = f'/{prefix!s}'
+
+    if prefix[-1] == '/':
+        prefix = prefix[:-1]
+
+    return prefix
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
@@ -16,18 +28,20 @@ def create_app(test_config=None):
 
     keycloak = keycloak_forward.KeyCloakForward(app)
 
-    @app.route('/health')
+    prefix = fix_prefix(os.environ.get('KEYCLOAK_URL_PREFIX', ''))
+
+    @app.route(f'{prefix!s}/health')
     def health():
         return ('Ping', 200)
 
-    @app.route('/keycloak')
+    @app.route(f'{prefix!s}/keycloak')
     def keycloak_entry():
         if keycloak.is_token_bearer(request.headers):
             return keycloak.bearer_authorized(request.headers, request.args)
 
         return keycloak.redirect_or_forward(session, request.headers, request.args)
 
-    @app.route('/keycloak/callback')
+    @app.route(f'{prefix!s}/keycloak/callback')
     def keycloak_callback():
         return keycloak.fetch_token_and_check_authorization(session, request.url)
 
